db33cb8db6cba7fe1bb6edd84fdfb658be2dbd8d
Mike Cardona <mikecardona076@gmail.com>
2021-05-09 13:34:22 -0700

    TERMINADO

---
+++ test3.py
@@ -0,0 +1,21 @@
+
+from geopy.geocoders import Nominatim
+import folium as fm
+
+geolocator = Nominatim(user_agent="mike")
+
+location1 = geolocator.geocode("Altiplano, tijuana")
+location2 = geolocator.geocode("Cerro colorado, tijuana")
+
+print(location1.address)
+
+mapa_mike = fm.Map(location=[location1.latitude,location1.longitude],
+            )
+
+fm.Marker([location1.latitude,location1.longitude], tooltip="Usted esta aqui").add_to(mapa_mike)
+fm.Marker([location2.latitude,location2.longitude], tooltip="Destino").add_to(mapa_mike)
+fm.RegularPolygonMarker(mapa_mike, fill_color='black', number_of_sides=3, radius=5,
+                                    rotation=angle2).add_to(mapa_mike)
+
+
+mapa_mike
